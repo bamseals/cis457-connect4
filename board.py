3e1cabe7793f4a7066b588c569cf78b10bdde38f
Zeratini <98341603+Zeratini@users.noreply.github.com>
2023-11-20 09:48:28 -0500

    Added 2 function skeletons

--- board.py
+++ board.py
@@ -11,5 +11,41 @@ class Board:
         for r in range(self.rows):
             board[r] = {}
             for c in range(self.cols):
-                board[r][c] = ''
+                board[r][c] = '[]'
         return board
+
+    def check_win(self, move, player):
+        """
+        :param move: A coordinate array, [x, y] of the location of the piece that was dropped
+        :param player: Which player made the move, as an int, 0 for red, 1 for blue
+        :return: Whether or not the move caused the player to win
+        """
+
+        ## TODO: Fill out win checks
+        x = move[0]
+        y = move[1]
+        win = False
+
+        # Horizontal Check
+        for i in range(x-3, x+4):
+            win = True
+        # Vertical Check
+        for i in range(y-3, y+4):
+            win = True
+
+        #Top left to bottom right diagonal
+        y += 3
+        for i in range(x-3, x+4):
+            win = True
+            y -= 1
+
+        # Bottom left to top right diagonal
+        x -= 3
+        for i in range(y - 3, y + 4):
+            win = True
+            x += 1
+
+        return win
+
+    def place_piece(self, column):
+        return
